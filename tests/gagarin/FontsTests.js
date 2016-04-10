c3cae645383a79b675e329e8d738c786a9c8ee2f
Abigail Watson <awatson1978@gmail.com>
2016-04-10 15:17:18 -0700

    fonts are fickle

--- tests/gagarin/FontsTests.js
+++ tests/gagarin/FontsTests.js
@@ -5,7 +5,8 @@ describe('clinical:fonts', function () {
   it('Application should use OpenSans or HelveticaNeue by default', function () {
     return client.execute(function () {
       //expect($('body').css('font-family')).to.equal('OpenSans, HelveticaNeue-Light, \'Helvetica Neue Light\', \'Helvetica Neue\', Helvetica, Arial, \'Lucida Grande\', sans-serif');
-      expect($('body').css('font-family')).to.have.string('Open Sans');
+      expect($('body').css('font-family')).to.have.string('Open');
+      expect($('body').css('font-family')).to.have.string('Sans');
       expect($('body').css('font-family')).to.have.string('Helvetica Neue');
       expect($('body').css('font-family')).to.have.string('Arial');
       expect($('body').css('font-family')).to.have.string('sans-serif');
