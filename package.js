c11c02f23dfbabbccde8fd9cc80040d9f79abb4e
Abigail Watson <awatson1978@gmail.com>
2016-10-15 21:44:52 -0500

    format(truetype)

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'clinical:fonts',
-  version: '1.1.2',
+  version: '1.1.3',
   summary: 'Fonts for healthcare applications - 3 of 9 barcode, fontawesome icons, and opensans.',
   git: 'http://github.com/clinical-meteor/clinical-fonts',
   documentation: 'README.md'
