41ddf92206af3d6d97565b8e83cdc4320ac1751a
Abigail Watson <awatson1978@gmail.com>
2020-02-11 02:36:40 -0600

    removing fontawesome icons; can be accessed via react-icons-kit

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'clinical:fonts',
-  version: '1.1.6',
+  version: '1.2.0',
   summary: 'Fonts for healthcare applications - 3 of 9 barcode, fontawesome icons, and opensans.',
   git: 'http://github.com/clinical-meteor/clinical-fonts',
   documentation: 'README.md'
@@ -10,8 +10,8 @@ Package.describe({
 Package.onUse(function (api) {
   api.versionsFrom('1.1.0.3');
 
-  api.use("fortawesome:fontawesome@4.4.0");
-  api.imply("fortawesome:fontawesome");
+  // api.use("fortawesome:fontawesome@4.4.0");
+  // api.imply("fortawesome:fontawesome");
 
   api.addFiles([
     'fonts/3OF9_NEW.TTF',
